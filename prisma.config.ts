f156ba2ffb49ef3044749b8d80c2b8c761015244
Abdullah Al Mamun <tapfer.abdullah@gmail.com>
2025-12-26 23:32:40 +0600

    verce.json added

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    ...(process.env["DATABASE_URL"]
+      ? { url: process.env["DATABASE_URL"] }
+      : {}),
   },
 });
